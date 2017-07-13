aa16599a225463334a2ee187e029f542ead3ab9a
daanlouter <daan.louter@gmail.com>
2017-07-13 11:59:46 +0100

    first commit

---
+++ artworks.js
@@ -0,0 +1,8 @@
+var artworks = [
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=20170713_2",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=20170713",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=20170712",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=05032017",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=22012017",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=21012017"
+]
