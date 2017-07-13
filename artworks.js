2d7b7226f2109c7edc3d3ac43c45340e45b854b1
Daan Louter <daan.louter@gmail.com>
2017-07-13 15:49:42 +0100

    Update artworks.js

--- artworks.js
+++ artworks.js
@@ -5,5 +5,8 @@ var artworks = [
 	"https://daanlouter.github.io/generative-sketchbook/?sketch=05032017",
 	"https://daanlouter.github.io/generative-sketchbook/?sketch=22012017",
 	"https://daanlouter.github.io/generative-sketchbook/?sketch=21012017",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=20170713_3",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=20170713_4",
+	"https://daanlouter.github.io/generative-sketchbook/?sketch=20170713_5",
 	"https://datatodisplay.com/scrapbook/projector-fountain/"
 ]
